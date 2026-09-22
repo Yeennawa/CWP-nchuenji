24c3c0eaae983f39b9aeb35c99a6c1a3b9bbde13
Yeennawa <nwminthrchuncitr78@gmail.com>
2026-09-22 22:38:17 +0700

    cell00

---
+++ cell00/ex00/name.py
@@ -0,0 +1,3 @@
+first_name="Nawamin"
+last_name="Chuenjit"
+print(first_name, last_name)
---
+++ cell00/ex01/name.py
@@ -0,0 +1,4 @@
+first_name="Nawamin"
+last_name="Chuenjit"
+whole_name=first_name + " " + last_name
+print(whole_name)
