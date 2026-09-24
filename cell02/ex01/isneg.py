e30172c7ecf358dc3db6e0ff5913cd8c3f23eaa2
Yeennawa <nwminthrchuncitr78@gmail.com>
2026-09-24 10:27:39 +0700

    update

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+number=input("")
+
+if(number<0):
+    print("This number is negative.")
+elif(number>0):
+    print("This number is positive.")
+elif(number==0):
+    print("This number is both positive and negative.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+Password=input("")
+password = "Python is awesome"
+if(password==Password):
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED.")
